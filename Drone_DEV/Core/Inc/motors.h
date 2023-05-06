b892fb774606da4e240cfafe404cbf166e7af5a4
Tobi <tibuoye@yahoo.co.uk>
2023-05-06 18:20:02 +0100

    Initial commit

---
+++ Drone_DEV/Core/Inc/motors.h
@@ -0,0 +1,57 @@
+/*
+ * motors.h
+ *
+ *  Created on: 5 May 2023
+ *      Author: tobii
+ */
+
+#ifndef INC_MOTORS_H_
+#define INC_MOTORS_H_
+
+#include "stdint.h"
+#include "stm32f1xx.h"
+
+enum motorPosition{
+	FrontLeft,
+	FrontRight,
+	RearLeft,
+	RearRight
+};
+
+enum state{
+	Off,
+	On
+};
+class Motors {
+public:
+	Motors(TIM_HandleTypeDef* pwm_timer);
+	void setOutput(motorPosition pos, uint16_t output);
+	uint16_t getOutput(motorPosition pos);
+	void command(state state);
+	void callibrate();
+	void setMinDutyCycle(float dutyCycle);
+	void setMaxDutyCycle(float dutyCycle);
+
+
+private:
+	uint16_t _output[4];
+	TIM_HandleTypeDef* _pwmTimer;
+	uint16_t _counterPeriod;
+	float _maxDutyCycle = 0.1;
+	float _minDutyCycle = 0.05;
+	uint16_t _maxRCInput = 2000;
+	uint16_t _minRCInput = 1000;
+
+	void set_registers();
+	uint16_t mapMotors(uint16_t value);
+	void zeroOutput();
+
+
+
+
+
+};
+
+
+
+#endif /* INC_MOTORS_H_ */
---
+++ Drone_DEV/Core/Src/motors.cpp
@@ -0,0 +1,106 @@
+/*
+ * motors.cpp
+ *
+ *  Created on: 5 May 2023
+ *      Author: tobii
+ */
+
+#include "motors.h"
+
+/**
+ * Constructor for Motor class
+ * @param pwm_timer set which timer motors use
+ */
+Motors::Motors(TIM_HandleTypeDef *pwm_timer):_pwmTimer(pwm_timer) {
+    HAL_TIM_PWM_Start(_pwmTimer, TIM_CHANNEL_1);
+    HAL_TIM_PWM_Start(_pwmTimer, TIM_CHANNEL_2);
+    HAL_TIM_PWM_Start(_pwmTimer, TIM_CHANNEL_3);
+    HAL_TIM_PWM_Start(_pwmTimer, TIM_CHANNEL_4);
+    _counterPeriod = _pwmTimer -> Instance -> ARR;
+}
+
+/**
+ * set Motor output level
+ * @param pos The position of the motor on the drone
+ * @param output The motor output level
+ */
+void Motors::setOutput(motorPosition pos, uint16_t output) {
+	_output[pos] = output;
+}
+
+/**
+ * get Motor output level
+ * @param pos The position of the motor on the drone
+ * @return The motor output level
+ */
+uint16_t Motors::getOutput(motorPosition pos) {
+	return _output[pos];
+}
+
+/**
+ * Pushes the set motor output to the motors
+ * @param state Turns the motors on or off
+ */
+void Motors::command(state state) {
+	if (state){
+		set_registers();
+	}
+	else {
+		zeroOutput();
+		set_registers();
+
+	}
+}
+
+void Motors::callibrate() {
+}
+
+/**
+ * Sets timer registers to produce correct output PWM
+ */
+void Motors::set_registers() {
+	_pwmTimer -> Instance -> CCR1 = mapMotors(_output[FrontRight]);
+	_pwmTimer -> Instance -> CCR2 = mapMotors(_output[RearLeft]);
+	_pwmTimer -> Instance -> CCR3 = mapMotors(_output[FrontLeft]);
+	_pwmTimer -> Instance -> CCR4 = mapMotors(_output[RearRight]);
+}
+
+/**
+ * Utility function to map RC values to correct values for motor PWM
+ * @param value input value to be converted (should be between 2000 and 1000)
+ * @return Mapped output
+ */
+uint16_t Motors::mapMotors(uint16_t value) {
+	if(value > 2000){
+		return _counterPeriod * _maxDutyCycle;
+	}
+	if (value < 1000){
+		return _counterPeriod * _minDutyCycle;
+	}
+	 return (value - _minRCInput) * (_counterPeriod* (_maxDutyCycle - _minDutyCycle)) / (_maxRCInput - _minRCInput) + (_counterPeriod * _minDutyCycle);
+}
+
+/**
+ * Set minimum PWM duty cycle
+ * @param dutyCycle
+ */
+void Motors::setMinDutyCycle(float dutyCycle) {
+	_minDutyCycle = dutyCycle;
+}
+
+/**
+ * set maximum PWM duty cycle
+ * @param dutyCycle
+ */
+void Motors::setMaxDutyCycle(float dutyCycle) {
+	_maxDutyCycle = dutyCycle;
+}
+
+/**
+ * Zero motor outputs
+ */
+void Motors::zeroOutput() {
+	for (uint8_t i = 0; i < sizeof(_output); i++){
+	        _output[i] = _minRCInput ;
+	    }
+}
