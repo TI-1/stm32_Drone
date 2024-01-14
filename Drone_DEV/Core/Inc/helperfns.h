fec0f73234e660d95e48bdf77e3a57d39ce12868
Tobi <tibuoye@yahoo.co.uk>
2024-01-14 16:28:23 +0000

    add map function

--- Drone_DEV/Core/Inc/helperfns.h
+++ Drone_DEV/Core/Inc/helperfns.h
@@ -22,6 +22,11 @@ bool floatComparison(float x,float y){
 	}
 }
 
+template <typename T> T map(T value, T in_min, T in_max, T out_min, T out_max) {
+
+		  return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+		}
+
 
 
 
