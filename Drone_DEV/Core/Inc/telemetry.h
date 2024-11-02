753678bca56462659bc4ef6a1eef5a00e0a8e417
Tobi <tibuoye@yahoo.co.uk>
2024-11-02 16:23:40 +0000

    Update Telemetry class

--- Drone_DEV/Core/Inc/telemetry.h
+++ Drone_DEV/Core/Inc/telemetry.h
@@ -8,23 +8,25 @@
 #ifndef SRC_TELEMETRY_H_
 #define SRC_TELEMETRY_H_
 #include "stdint.h"
+#include <stdio.h>
 #include "usart.h"
+#include "imu.h"
+#include <Mavlink/common/mavlink.h>
 class Telemetry{
-	public:
-		static Telemetry *getInstance();
-		Telemetry(const Telemetry&) = delete;
-		Telemetry& operator=(const Telemetry&) = delete;
-		void sendData(uint8_t databuf[]);
-		static void setDefaultUART(UART_HandleTypeDef& uartHandle);
-
-	private:
-		static Telemetry* _telemetry;
-		Telemetry();
-		~Telemetry() {}
-		 UART_HandleTypeDef& huart;
-		 static UART_HandleTypeDef* defaultUART;
-
-
+public:
+    Telemetry(UART_HandleTypeDef *telem, uint32_t frequency);
+    Telemetry(const Telemetry& t) = delete;
+    Telemetry& operator=(const Telemetry &obj) = delete;
+    void gatherData(IMU *imu);
+    void update(); // Call this in the loop
+    void sendMessage(uint8_t message_code, uint8_t value);
+private:
+    UART_HandleTypeDef *_telemHuart;
+    uint32_t _lastSendTime;
+    uint32_t _sendInterval; // Time in milliseconds
+    uint32_t _startTime = HAL_GetTick();
+    char gyrobuf[40];
+    void sendData();
 };
 
 #endif /* SRC_TELEMETRY_H_ */
--- Drone_DEV/Core/Src/telemetry.cpp
+++ Drone_DEV/Core/Src/telemetry.cpp
@@ -6,24 +6,32 @@
  */
 #include "telemetry.h"
 
+Telemetry::Telemetry(UART_HandleTypeDef *telem, uint32_t frequency) {
+	_telemHuart = telem;
+	_sendInterval = 1000 / frequency;
+	_lastSendTime = HAL_GetTick();
+}
 
-Telemetry* Telemetry::_telemetry = nullptr;
-UART_HandleTypeDef* Telemetry::defaultUART = &huart2;
+void Telemetry::gatherData(IMU *imu) {
+	uint32_t dataTime = (HAL_GetTick() - _startTime) / 1000;
+	sprintf(gyrobuf,"*%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%d*"
+			,imu->Gyro(X),imu->Gyro(Y),imu->Gyro(Z),imu->Yaw(),imu->Pitch(),imu->Roll(), (int)dataTime);
+}
 
-Telemetry::Telemetry():huart(*defaultUART) {}
+void Telemetry::sendMessage(uint8_t message_code, uint8_t value){
+	char message_buf[40];
+	sprintf(message_buf,"#%d,%d,%d,%d,%d,%d#",message_code, value, 0, 0, 0,0);
+		HAL_UART_Transmit(_telemHuart, (uint8_t *)message_buf, sizeof(message_buf), 1000);
+}
 
-Telemetry* Telemetry::getInstance() {
-	if (_telemetry == nullptr){
-		_telemetry = new Telemetry();
+void Telemetry::update() {
+	uint32_t currentTime = HAL_GetTick();
+	if (currentTime - _lastSendTime >= _sendInterval){
+		sendData();
+		_lastSendTime = currentTime;
 	}
-	return _telemetry;
-
 }
 
-
-void Telemetry::sendData(uint8_t databuf[]) {
-	HAL_UART_Transmit(defaultUART, databuf, 8, 1000);
+void Telemetry::sendData(){
+	HAL_UART_Transmit(_telemHuart, (uint8_t *)gyrobuf, sizeof(gyrobuf), 1000);
 }
-
-
-
