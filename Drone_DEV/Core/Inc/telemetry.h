2ee6891128df100c61f310e41aadcc868a17d2a9
Tobi <tibuoye@yahoo.co.uk>
2024-12-10 22:41:01 +0000

    Update Telemetry

--- Drone_DEV/Core/Inc/telemetry.h
+++ Drone_DEV/Core/Inc/telemetry.h
@@ -11,22 +11,35 @@
 #include <stdio.h>
 #include "usart.h"
 #include "imu.h"
-#include <Mavlink/common/mavlink.h>
+#include "mavlink.h"
+
 class Telemetry{
 public:
     Telemetry(UART_HandleTypeDef *telem, uint32_t frequency);
     Telemetry(const Telemetry& t) = delete;
     Telemetry& operator=(const Telemetry &obj) = delete;
-    void gatherData(IMU *imu);
-    void update(); // Call this in the loop
+    void sendData(IMU *imu);
     void sendMessage(uint8_t message_code, uint8_t value);
+    void SendMavLinkMessage(mavlink_message_t *msg);
+    void setState(MAV_STATE state);
+    void sendHeartbeat();
+    void sendStatusText(const char* text, uint8_t severity);
+    UART_HandleTypeDef* getUartHandle();
+    void onReceiveComplete();
+    void processIncomingData(mavlink_param_set_t &paramSet);
 private:
     UART_HandleTypeDef *_telemHuart;
+    MAV_STATE _state = MAV_STATE_UNINIT;
     uint32_t _lastSendTime;
     uint32_t _sendInterval; // Time in milliseconds
-    uint32_t _startTime = HAL_GetTick();
-    char gyrobuf[40];
-    void sendData();
+    uint32_t _startTime;
+    uint8_t _mavlink_buffer[MAVLINK_MAX_PACKET_LEN];
+    uint8_t _recievebuffer[MAVLINK_MAX_PACKET_LEN];
+    uint8_t _tempbuffer[2];
+    uint16_t _writeIndex = 0;
+    uint16_t _readIndex = 0;
+    char _gyrobuf[100];
+
 };
 
 #endif /* SRC_TELEMETRY_H_ */
--- Drone_DEV/Core/Src/telemetry.cpp
+++ Drone_DEV/Core/Src/telemetry.cpp
@@ -9,29 +9,103 @@
 Telemetry::Telemetry(UART_HandleTypeDef *telem, uint32_t frequency) {
 	_telemHuart = telem;
 	_sendInterval = 1000 / frequency;
-	_lastSendTime = HAL_GetTick();
+	_startTime = HAL_GetTick();
+	_lastSendTime = _startTime;
+	HAL_UART_Receive_IT(telem, &_tempbuffer[0], 1);
 }
 
-void Telemetry::gatherData(IMU *imu) {
-	uint32_t dataTime = (HAL_GetTick() - _startTime) / 1000;
-	sprintf(gyrobuf,"*%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%d*"
-			,imu->Gyro(X),imu->Gyro(Y),imu->Gyro(Z),imu->Yaw(),imu->Pitch(),imu->Roll(), (int)dataTime);
-}
-
-void Telemetry::sendMessage(uint8_t message_code, uint8_t value){
-	char message_buf[40];
-	sprintf(message_buf,"#%d,%d,%d,%d,%d,%d#",message_code, value, 0, 0, 0,0);
-		HAL_UART_Transmit(_telemHuart, (uint8_t *)message_buf, sizeof(message_buf), 1000);
-}
-
-void Telemetry::update() {
+void Telemetry::sendData(IMU *imu) {
 	uint32_t currentTime = HAL_GetTick();
 	if (currentTime - _lastSendTime >= _sendInterval){
-		sendData();
+		uint32_t dataTime = (HAL_GetTick() - _startTime);
+		mavlink_message_t msg;
+		mavlink_msg_attitude_pack(
+			1, 200, &msg, dataTime,
+			imu->Roll() * M_PI/180, imu->Pitch() * M_PI / 180, imu->Yaw() * M_PI / 180,
+			imu->Gyro(X) * M_PI / 180, imu->Gyro(Y) * M_PI / 180, imu->Gyro(Z) * M_PI / 180
+		);
+		SendMavLinkMessage(&msg);
 		_lastSendTime = currentTime;
-	}
+		}
 }
 
-void Telemetry::sendData(){
-	HAL_UART_Transmit(_telemHuart, (uint8_t *)gyrobuf, sizeof(gyrobuf), 1000);
+void Telemetry::sendStatusText(const char* text, uint8_t severity) {
+	mavlink_message_t msg;
+	mavlink_msg_statustext_pack(1, 1, &msg, severity, text,1,0);
+
+	uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+	uint16_t len = mavlink_msg_to_send_buffer(buffer, &msg);
+	HAL_UART_Transmit(_telemHuart, buffer, len, HAL_MAX_DELAY);
+}
+
+void Telemetry::sendHeartbeat() {
+	mavlink_message_t msg;
+	mavlink_msg_heartbeat_pack(1, 1, &msg,
+		MAV_TYPE_QUADROTOR,  // Set type as needed
+		MAV_AUTOPILOT_GENERIC,
+		MAV_MODE_MANUAL_ARMED,
+		0,                   // Custom mode, can define modes here
+		_state);   // System state
+
+	uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+	uint16_t len = mavlink_msg_to_send_buffer(buffer, &msg);
+	HAL_UART_Transmit(_telemHuart, buffer, len, HAL_MAX_DELAY);
+}
+
+
+void Telemetry::SendMavLinkMessage(mavlink_message_t *msg) {
+	int len = mavlink_msg_to_send_buffer(_mavlink_buffer, msg);
+	HAL_UART_Transmit(_telemHuart, _mavlink_buffer, len, 1000);
+}
+
+
+
+
+void Telemetry::setState(MAV_STATE state) {
+	_state = state;
+}
+
+UART_HandleTypeDef* Telemetry::getUartHandle() {
+	return _telemHuart;
+}
+
+void Telemetry::onReceiveComplete() {
+	_recievebuffer[_writeIndex] = _tempbuffer[0];
+	_writeIndex = (_writeIndex + 1) % (MAVLINK_MAX_PACKET_LEN);
+
+	if(_writeIndex == _readIndex){
+		_readIndex = (_readIndex + 1) % (MAVLINK_MAX_PACKET_LEN);
+	}
+	HAL_UART_Receive_IT(_telemHuart, _tempbuffer , 1);
+}
+
+void Telemetry::processIncomingData(mavlink_param_set_t &paramSet) {
+	mavlink_message_t msg;
+	mavlink_status_t status;
+
+	while (_readIndex != _writeIndex){
+		uint8_t byte = _recievebuffer[_readIndex];
+		_readIndex = (_readIndex + 1) % (MAVLINK_MAX_PACKET_LEN);
+
+		if(mavlink_parse_char(MAVLINK_COMM_0, byte, &msg, &status)){
+			switch(msg.msgid){
+				case MAVLINK_MSG_ID_PARAM_SET:
+					// Example handling for PARAM_SET
+					mavlink_msg_param_set_decode(&msg, &paramSet);
+					mavlink_msg_param_value_pack(paramSet.target_system,
+							paramSet.target_component,
+							&msg,
+							paramSet.param_id,
+							paramSet.param_value,
+							paramSet.param_type,
+							1, 1);
+					SendMavLinkMessage(&msg);
+					return;
+					break;
+				default:
+					break;
+			}
+
+		}
+	}
 }
